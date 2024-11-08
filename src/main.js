83cde3deb39471f39fd8567ce205127fe6b86b88
yashujs <1686539697@qq.com>
2024-11-09 03:30:46 +0800

    v0.0.12024-11-9-3_30_45

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "modern-normalize/modern-normalize.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+app.use(createPinia());
+app.use(ElementPlus, { size: "default", zIndex: 3000 });
+app.mount("#app");
